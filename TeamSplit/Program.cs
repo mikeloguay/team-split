3f94869f108e4069dc64c8df2cc7686a89920650
Miguel Fernández <mikeloguay@gmail.com>
2025-06-01 00:16:34 +0200

    first version working

--- TeamSplit/Program.cs
+++ TeamSplit/Program.cs
@@ -2,8 +2,19 @@
 
 List<Player> players =
 [
-    new Player { Name = "Miki", Level = 10},
-    new Player { Name = "Ale", Level = 10},
-    new Player { Name = "Antonio", Level = 10},
+    new Player { Name = "Roberto", Level = 1},
+    new Player { Name = "Diego", Level = 60},
+    new Player { Name = "Ale", Level = 80},
+    new Player { Name = "Miki", Level = 80},
+    new Player { Name = "Antonio", Level = 20},
+    new Player { Name = "Canijo", Level = 100},
+    new Player { Name = "Dani", Level = 75},
+    new Player { Name = "Tito", Level = 40},
+    new Player { Name = "Jose", Level = 75},
+    new Player { Name = "DaniJ", Level = 60},
 ];
 
+ITeamSplitter teamSplitter = new TeamSplitter();
+Versus versus = teamSplitter.Split([.. players]);
+
+Console.WriteLine(versus);
--- TeamSplit/Versus.cs
+++ TeamSplit/Versus.cs
@@ -5,4 +5,6 @@ public class Versus
     public required Team Team1 { get; init; }
     public required Team Team2 { get; init; }
     public int LevelDiff => Math.Abs(Team1.Level - Team2.Level);
+
+    public override string ToString() => $"{Team1} \n {Team2}";
 }
