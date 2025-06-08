a79a112c969bf5a89968aeeda19ad6daf079dca5
Miguel Fernández <mikeloguay@gmail.com>
2025-06-08 22:58:25 +0200

    cors fix

--- backend/TeamSplit.Api/Program.cs
+++ backend/TeamSplit.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "https://team-split-site.onrender.com/",
+                "https://team-split.onrender.com/",
                 "http://localhost:7070"
             )
             .WithMethods("GET", "POST")
