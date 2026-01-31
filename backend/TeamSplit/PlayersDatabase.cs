cc5e253511642296bb7f8ebcaa406e09287c2198
Miguel Fernández <mikeloguay@gmail.com>
2026-01-31 20:20:09 +0100

    Update player levels in PlayersDatabase
    Updated the level of player 'Dani' from 60 to 61 and 'Canca' from 45 to 39.

--- backend/TeamSplit/PlayersDatabase.cs
+++ backend/TeamSplit/PlayersDatabase.cs
@@ -10,14 +10,14 @@ public static class PlayersDatabase
         new Player { Name = "Miki", Level = 74 },
         new Player { Name = "Antonio", Level = 25 },
         new Player { Name = "Canijo", Level = 100 },
-        new Player { Name = "Dani", Level = 65 },
+        new Player { Name = "Dani", Level = 61 },
         new Player { Name = "Tito", Level = 36 },
         new Player { Name = "Jose", Level = 75 },
         new Player { Name = "DaniJ", Level = 66 },
         new Player { Name = "Juani", Level = 75 },
         new Player { Name = "Pablo", Level = 40 },
-        new Player { Name = "Canca", Level = 45 },
-        new Player { Name = "Alberto", Level = 67 },
+        new Player { Name = "Canca", Level = 39 },
+        new Player { Name = "Alberto", Level = 71 },
         new Player { Name = "Iván", Level = 38 },
         new Player { Name = "Manolo", Level = 72 },
         new Player { Name = "Isra", Level = 78 },
@@ -28,5 +28,6 @@ public static class PlayersDatabase
         new Player { Name = "Rafa", Level = 65 },
      ];
 }
+
 
 
